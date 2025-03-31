02eb3b7941101b35f79fba2692cddf3e7fc2735b
hgck000 <vlight78964@gmail.com>
2025-03-31 20:11:54 +0700

    Code drag drop for trello-web (deskop and mobile)

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -2,17 +2,71 @@ import Box from '@mui/material/Box'
 import ListColumns from './ListColumns/ListColumns'
 import { mapOrder } from '~/utils/sorts'
 
+import {
+  DndContext,
+  // PointerSensor,
+  MouseSensor,
+  TouchSensor,
+  useSensor,
+  useSensors
+} from '@dnd-kit/core'
+import { arrayMove } from '@dnd-kit/sortable'
+import { useEffect, useState } from 'react'
+
+
 function BoardContent({ board }) {
-  const orderedColumns = mapOrder(board?.columns, board?.columnOrderIds, '_id')
+  // const pointerSensor = useSensor(PointerSensor, { activationConstraint: { distance: 10 } })
+
+  // Yêu cầu chuột di chuyển 10px thì mới kích hoạt event, fix trường hợp click bị gọi event
+  const mouseSensor = useSensor(MouseSensor, { activationConstraint: { distance: 10 } })
+
+  // Nhấn giữ 250ms và dung sai của cảm ứng 5px thì mới kích hoạt event
+  const touchSensor = useSensor(TouchSensor, { activationConstraint: { delay: 250, tolerance: 5 } })
+
+  //Ưu tiên sử dụng kết hợp 2 loại sensor mouse và touch để có trai nghiệm trên mobile tốt nhất
+  const sensors = useSensors(mouseSensor, touchSensor)
+
+  const [orderedColumns, setOrderedColumns] = useState([])
+
+  useEffect(() => {
+    setOrderedColumns(mapOrder(board?.columns, board?.columnOrderIds, '_id'))
+  }, [board])
+
+  const handleDragEnd = (event) => {
+    // console.log('handleDragEnd: ', event)
+    const { active, over } = event
+
+    if (!over) return
+
+    if (active.id !== over.id) {
+      // Lấy vị trí cũ (từ thằng active)
+      const oldIndex = orderedColumns.findIndex(c => c._id === active.id)
+      // Lấy vị trí cũ (từ thằng cover)
+      const newIndex = orderedColumns.findIndex(c => c._id === over.id)
+
+
+      // Dùng arrayMove của thằng dnđ-kit để sắp xếp lại mảng Columns ban đầu
+      const dndOrderedColumns = arrayMove(orderedColumns, oldIndex, newIndex)
+      // 2 cái console.log dữ liệu này sau dùng để xử lí APi
+      // const dndOrderedColmnsIds = dndOrderedColumns.map(c => c._id)
+      // console.log('dndOrderedColmns: ', dndOrderedColumns)
+      // setOrderedColumns(dndOrderedColumns)
+
+      setOrderedColumns(dndOrderedColumns)
+    }
+  }
+
   return (
-    <Box sx={{
-      bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#2c3e50' : '#1565c0'),
-      width: '100%',
-      height: (theme) => theme.trello.boardContentHeight,
-      p: '10px 0'
-    }}>
-      <ListColumns columns={orderedColumns} />
-    </Box>
+    <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
+      <Box sx={{
+        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#2c3e50' : '#1565c0'),
+        width: '100%',
+        height: (theme) => theme.trello.boardContentHeight,
+        p: '10px 0'
+      }}>
+        <ListColumns columns={orderedColumns} />
+      </Box>
+    </DndContext>
   )
 }
 
