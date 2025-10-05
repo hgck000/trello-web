98fdb9e4b2f22e39a84022decaec13d2e0029e6b
minh <vlight78964@gmail.com>
2025-10-05 17:06:37 +0700

    Dnd columns

--- src/apis/index.js
+++ src/apis/index.js
@@ -8,6 +8,11 @@ export const fetchBoarDeatilsAPI = async (boardId) => {
   return response.data
 }
 
+export const updateBoardDeatilsAPI = async (boardId, updateData) => {
+  const response = await axios.put(`${API_ROOT}/v1/boards/${boardId}`, updateData)
+  return response.data
+}
+
 /** Column */
 export const createNewColumnAPI = async (newColumnData) => {
   const response = await axios.post(`${API_ROOT}/v1/columns`, newColumnData)
